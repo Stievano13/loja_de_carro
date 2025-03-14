6813e9e549d5546129e7be4f36e733ee79d2b501
Stievano13 <159502098+Stievano13@users.noreply.github.com>
2025-03-13 21:49:56 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './styles/App.css';
 function App() {
   return (
     <div className="container">
-      <img src="/images/impactateste.png" alt="Logo Impacta" className="logo" width="150"/>
+      <img src="/images/agulhasnegras.png" alt="Logo Agulhas" className="logo" width="150"/>
       <header className="catalog-header">
-        <h1>Testando os produtos</h1>
+        <h1>Compras de Veiculos</h1>
         <ul className="product-list">
-          <li className="product-item">1 - 50$ <img src="/images/teste1.png" alt="1" width="50" /></li>
-          <li className="product-item">2 - 10$</li>
-          <li className="product-item">3 - 15$</li>
+          <li className="product-item"></li>
+          <li className="product-item"></li>
+          <li className="product-item"></li>
         </ul>
       </header>
     </div>
